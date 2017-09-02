3587f6bcd3af84a01c4b7e1916ecfeeafec630db
softprops <d.tangren@gmail.com>
2017-09-02 15:04:36 -0400

    annotate nginx status with gce metadata

--- datadog-transformer.rb
+++ datadog-transformer.rb
@@ -1,8 +1,22 @@
+require 'net/http'
 require 'yaml'
 
 class JSONTransformer
   def initialize
     @config = File.exists?("/opt/nginx_dogstats.yaml") ? YAML.load_file("/opt/nginx_dogstats.yaml") : {}
+    @gce_zone = self.resolve_gce_zone
+  end
+
+  def resolve_gce_zone
+    uri = URI("http://metadata.google.internal/computeMetadata/v1/instance/zone")
+    req = Net::HTTP::Get.new(uri)
+    req['Metadata-Flavor'] = 'Google'
+    begin
+      resp =  Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(req) }
+      resp.body.split("/").last
+    rescue Exception => msg
+      puts "failed to request instance zone: #{msg}"
+    end
   end
 
   def path_tag(path)
@@ -23,6 +37,12 @@ class JSONTransformer
       else
         {}
       end
+    gce_metadata =
+      if @gce_zone
+        { "gce_zone" => @gce_zone }
+      else
+        {}
+      end
 
     upstream_time = {}
     # capture upstream response time, only if it's defined
@@ -40,7 +60,7 @@ class JSONTransformer
         "kube_namespace" => json["kubernetes"]["namespace_name"],
         "container_name" => json["kubernetes"]["container_name"],
         "pod_name" => json["kubernetes"]["namespace_name"] + "/" + json["kubernetes"]["pod_name"]
-      }.merge(labels)
+      }.merge(labels).merge(gce_metadata)
     }.merge(upstream_time)
 
     return record
